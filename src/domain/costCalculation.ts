729f47ea7dade4feed52c293f0bf8d343e6eafdc
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-05-29 17:24:18 +0200

    :sparkles: Add domain logic to calculate current cost of electricity

---
+++ src/domain/costCalculation.ts
@@ -0,0 +1,33 @@
+export enum CostLevel {
+  low = 'low',
+  medium = 'medium',
+  high = 'high',
+}
+
+export const NATIONAL_HOLIDAYS = [
+  'Fri Jan 01 2021',
+  'Wed Jan 06 2021',
+  'Fri Apr 02 2021',
+  'Sat May 01 2021',
+  'Tue Oct 12 2021',
+  'Mon Nov 01 2021',
+  'Mon Dec 06 2021',
+  'Wed Dec 08 2021',
+  'Sat Dec 25 2021',
+]
+
+export const getCurrentCost = (currentTime: Date): CostLevel => {
+  const hours = currentTime.getHours()
+  const weekDay = currentTime.getDay()
+  const dateWithoutTime = currentTime.toDateString()
+
+  const isWeekend = weekDay === 0 || weekDay === 6
+  if (isWeekend) return CostLevel.low
+
+  const isNationalHoliday = NATIONAL_HOLIDAYS.includes(dateWithoutTime)
+  if (isNationalHoliday) return CostLevel.low
+
+  if ((hours > 9 && hours < 14) || (hours > 17 && hours < 22)) return CostLevel.high
+  if (hours > 7) return CostLevel.medium
+  return CostLevel.low
+}
---
+++ tests/domain/costCalculation.unit.test.ts
@@ -0,0 +1,82 @@
+import { CostLevel, getCurrentCost } from '@src/domain/costCalculation'
+describe('costCalculation', () => {
+  describe('getCurrentCost', () => {
+    it('returns a low cost level when the time is in the low cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T00:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.low)
+
+      const inBetweenTime = new Date('2021-05-27T03:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.low)
+
+      const higherLimitTime = new Date('2021-05-27T07:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.low)
+    })
+
+    it('returns a medium cost level when the time is in the first medium cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T08:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.medium)
+
+      const inBetweenTime = new Date('2021-05-27T09:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.medium)
+
+      const higherLimitTime = new Date('2021-05-27T09:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.medium)
+    })
+
+    it('returns a medium cost level when the time is in the second medium cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T14:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.medium)
+
+      const inBetweenTime = new Date('2021-05-27T15:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.medium)
+
+      const higherLimitTime = new Date('2021-05-27T17:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.medium)
+    })
+
+    it('returns a medium cost level when the time is in the third medium cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T22:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.medium)
+
+      const inBetweenTime = new Date('2021-05-27T22:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.medium)
+
+      const higherLimitTime = new Date('2021-05-27T23:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.medium)
+    })
+
+    it('returns a high cost level when the time is in the first high cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T10:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.high)
+
+      const inBetweenTime = new Date('2021-05-27T12:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.high)
+
+      const higherLimitTime = new Date('2021-05-27T13:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.high)
+    })
+
+    it('returns a high cost level when the time is in the second high cost time slot', () => {
+      const lowerLimitTime = new Date('2021-05-27T18:00:00')
+      expect(getCurrentCost(lowerLimitTime)).toEqual(CostLevel.high)
+
+      const inBetweenTime = new Date('2021-05-27T19:30:00')
+      expect(getCurrentCost(inBetweenTime)).toEqual(CostLevel.high)
+
+      const higherLimitTime = new Date('2021-05-27T21:59:00')
+      expect(getCurrentCost(higherLimitTime)).toEqual(CostLevel.high)
+    })
+
+    it('returns a low cost level when is weekend', () => {
+      const date = new Date('2021-05-29T20:00:00')
+      const result = getCurrentCost(date)
+      expect(result).toEqual(CostLevel.low)
+    })
+
+    it('returns a low cost level when is national holiday', () => {
+      const date = new Date('2021-01-01T10:00:00Z')
+      const result = getCurrentCost(date)
+      expect(result).toEqual(CostLevel.low)
+    })
+  })
+})
