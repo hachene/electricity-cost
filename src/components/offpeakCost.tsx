9e29d7c95c606882709cdebb9568e1aa41a61eca
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-05-29 15:35:32 +0200

    :sparkles: Add OffPeakCost component

---
+++ src/components/offpeakCost.tsx
@@ -0,0 +1,19 @@
+import layoutStyles from '@src/styles/layout.module.css'
+import utilStyles from '@src/styles/utils.module.css'
+import Image from 'next/image'
+
+export const OffpeakCost = () => (
+  <div className={`${layoutStyles.semaphoreContainer} ${layoutStyles.greenSemaphore}`}>
+    <Image
+      priority
+      src="/images/face_thinking_person_woman.png"
+      className={utilStyles.borderCircle}
+      height={128}
+      width={108}
+      alt={'ilustración de una moneda'}
+    />
+
+    <h1 className={utilStyles.heading2Xl}>¡Barata!</h1>
+    <h2 className={utilStyles.headingXl}>Hora valle</h2>
+  </div>
+)
