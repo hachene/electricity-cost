d14d19911dc1d8dd276a6b592bf943ba08714da9
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-06-01 00:20:28 +0200

    :white_check_mark: Fix index test by passing correct prop to Home component

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,25 +1,22 @@
+import { CostLevel } from '@src/domain/costCalculation'
 import Home from '@src/pages'
 import { render, screen } from '@testing-library/react'
-import * as Utils from '@src/lib/utils'
 
 describe('index', () => {
   it('renders the offpeak message when it is offpeak cost time', () => {
-    jest.spyOn(Utils, 'getToday').mockReturnValue(new Date('2021-05-27T03:00:00'))
-    render(<Home />)
+    render(<Home costLevel={CostLevel.low} />)
     const renderedMessage = screen.queryByText(/Poco/)
     expect(renderedMessage).toBeVisible()
   })
 
   it('renders the medium message when it is a medium cost time', () => {
-    jest.spyOn(Utils, 'getToday').mockReturnValue(new Date('2021-05-27T08:00:00'))
-    render(<Home />)
+    render(<Home costLevel={CostLevel.medium} />)
     const renderedMessage = screen.queryByText(/Ni mucho ni poco/)
     expect(renderedMessage).toBeVisible()
   })
 
   it('renders the peak message when it is peak cost time', () => {
-    jest.spyOn(Utils, 'getToday').mockReturnValue(new Date('2021-05-27T11:00:00'))
-    render(<Home />)
+    render(<Home costLevel={CostLevel.high} />)
     const renderedMessage = screen.queryByText(/Mucho/)
     expect(renderedMessage).toBeVisible()
   })
