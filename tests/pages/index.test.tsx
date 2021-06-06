7b21f6baf260c9488d6c4cfb60c431fccec40e20
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-06-06 10:32:34 +0200

    :white_check_mark: Fix index page test changing Home prop to the new currentCost one

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -4,19 +4,19 @@ import { render, screen } from '@testing-library/react'
 
 describe('index', () => {
   it('renders the offpeak message when it is offpeak cost time', () => {
-    render(<Home costLevel={CostLevel.low} />)
+    render(<Home currentCost={CostLevel.low} />)
     const renderedMessage = screen.queryByText(/Poco/)
     expect(renderedMessage).toBeVisible()
   })
 
   it('renders the medium message when it is a medium cost time', () => {
-    render(<Home costLevel={CostLevel.medium} />)
+    render(<Home currentCost={CostLevel.medium} />)
     const renderedMessage = screen.queryByText(/Ni mucho ni poco/)
     expect(renderedMessage).toBeVisible()
   })
 
   it('renders the peak message when it is peak cost time', () => {
-    render(<Home costLevel={CostLevel.high} />)
+    render(<Home currentCost={CostLevel.high} />)
     const renderedMessage = screen.queryByText(/Mucho/)
     expect(renderedMessage).toBeVisible()
   })
