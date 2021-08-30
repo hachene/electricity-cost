a15f5b3a9fc6f12d76786a0da13dc2e27f57e948
Juan Martinez <jmp.juan@gmail.com>
2021-08-30 20:56:40 +0200

    :white_check_mark: Fix tests

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,23 +1,37 @@
-import { CostLevel } from '@src/domain/costCalculation'
 import Home from '@src/pages'
 import { render, screen } from '@testing-library/react'
+import * as costCalculation from '@src/domain/costCalculation'
 
 describe('index', () => {
   describe('cost components', () => {
+    afterEach(() => jest.restoreAllMocks())
+
     it('renders the offpeak message when it is offpeak cost time', () => {
-      render(<Home currentCost={CostLevel.low} />)
+      const mock = jest.spyOn(costCalculation, 'getCurrentCost')
+      mock.mockReturnValue(costCalculation.CostLevel.low)
+
+      render(<Home />)
+
       const renderedMessage = screen.queryByText(/Poco/)
       expect(renderedMessage).toBeVisible()
     })
 
     it('renders the medium message when it is a medium cost time', () => {
-      render(<Home currentCost={CostLevel.medium} />)
+      const mock = jest.spyOn(costCalculation, 'getCurrentCost')
+      mock.mockReturnValue(costCalculation.CostLevel.medium)
+
+      render(<Home />)
+
       const renderedMessage = screen.queryByText(/Ni mucho ni poco/)
       expect(renderedMessage).toBeVisible()
     })
 
     it('renders the peak message when it is peak cost time', () => {
-      render(<Home currentCost={CostLevel.high} />)
+      const mock = jest.spyOn(costCalculation, 'getCurrentCost')
+      mock.mockReturnValue(costCalculation.CostLevel.high)
+
+      render(<Home />)
+
       const renderedMessage = screen.queryByText(/Mucho/)
       expect(renderedMessage).toBeVisible()
     })
@@ -25,8 +39,7 @@ describe('index', () => {
 
   describe('powered by component', () => {
     it('renders the "powered by" message', () => {
-      render(<Home currentCost={CostLevel.low} />)
-      screen.debug()
+      render(<Home />)
       const renderedMessage = screen.queryByText(/Tecnología punta diseñada por/)
       expect(renderedMessage).toBeVisible()
     })
