26eaabff610128a3195111a6b66931d8af50af58
Juan Martinez <jmp.juan@gmail.com>
2021-08-30 20:18:41 +0200

    :sparkles: Add custom hook to use setInterval in a declarative manner

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,26 @@
+import { useEffect, useRef } from 'react'
+
 export function getToday(): Date {
   return new Date()
+}
+
+// Custom hook extracted from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback: Function, delay: number) {
+  const savedCallback = useRef<Function>()
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
 }
